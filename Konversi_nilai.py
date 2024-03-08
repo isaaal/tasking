b16e34bf6e02ada2a2541b031906773429f5bdb5
isaaal <160223903+isaaal@users.noreply.github.com>
2024-03-08 11:30:02 +0700

    Add files via upload

---
+++ Konversi_nilai.py
@@ -0,0 +1,14 @@
+konversi = int(input("masukan nilai: "))
+
+if konversi <=50:
+    A ="E"
+elif  konversi <=60:
+    A="D"
+elif konversi <=70:
+    A = "C"
+elif konversi <=80:
+    A = "B"
+else:
+    A = "A"
+
+print(A)
